17d7ee860478a5d07be1930ef736562703db22f7
Tom Nielsen <tanielsen@gmail.com>
2015-03-19 14:53:09 +0000

    count in getFollowedBy

--- src/Instagram/Relationships.hs
+++ src/Instagram/Relationships.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts, OverloadedStrings #-}
 -- | relationship handling
 -- <http://instagram.com/developer/endpoints/relationships/#>
 module Instagram.Relationships (
@@ -14,6 +14,7 @@ import Instagram.Monad
 import Instagram.Types
 
 import Data.Typeable (Typeable)
+import Data.String
 
 import qualified Network.HTTP.Types as HT
 import Data.Char (toLower)
@@ -27,9 +28,13 @@ getFollows uid token  =getGetEnvelopeM ["/v1/users/",uid,"/follows"] token ([]::
 
 -- | Get the list of users this user is followed by.
 getFollowedBy ::     (MonadBaseControl IO m, MonadResource m) => UserID
+  -> Maybe Int
   -> Maybe OAuthToken
   -> InstagramT m (Envelope [User])
-getFollowedBy uid token  =getGetEnvelopeM ["/v1/users/",uid,"/followed-by"] token ([]::HT.Query)
+getFollowedBy uid Nothing token  =getGetEnvelopeM ["/v1/users/",uid,"/followed-by"] token ([]::HT.Query)
+getFollowedBy uid (Just count) token  =getGetEnvelopeM ["/v1/users/",uid,"/followed-by"]
+                                                       token
+                                                       ([("count", Just $ fromString $ show count)]::HT.Query)
 
 -- | List the users who have requested this user's permission to follow.
 getRequestedBy ::     (MonadBaseControl IO m, MonadResource m) =>
